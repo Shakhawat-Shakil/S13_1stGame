50205de6c097b389a9c6dfbc54ce78b75d239da4
Shakhawat-Shakil <96696222+Shakhawat-Shakil@users.noreply.github.com>
2022-07-11 11:51:45 +0600

    Add files via upload

---
+++ S13_2ndGame.py
@@ -0,0 +1,14 @@
+#Guessing Game
+from random import randint
+randVal = randint(0, 100)
+
+
+while True:
+    guess = int(input("Enter a random Number between 0 and 100\n"))
+    if guess == randVal:
+        break
+    elif guess < randVal:
+        print("Your guess too low!")
+    else:
+        print("Your guess too high! ")
+print("Your guess is correct")
